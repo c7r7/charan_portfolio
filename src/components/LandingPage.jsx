1a1172b18bd6fc4a6d422e209734785059380068
Charan <charansomalraju.com>
2024-08-27 15:20:43 -0400

    Landing_page Change

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -28,7 +28,7 @@ const LandingPage = () => {
                         <Cursor cursorStyle="|" />
                     </div>
                     <div className="download-cv">
-                        <a href="https://drive.google.com/file/d/1jB9GbnB03kBiXKf1uvmRRN2REwVH_0C5/view?usp=drive_link" className="button" download="Charan Resume.pdf">
+                        <a href="https://drive.google.com/file/d/175HcmmXg9GzI5OIgVblx-fP_PDajff7c/view?usp=drive_link" className="button" download="Charan Resume.pdf">
                             <FiDownload className="icon" />
                             {' '}Download CV
                         </a>
